60af3f49195775877af70810d3e6f170089ebfee
xGeek <assel.meher@gmail.com>
2014-06-08 00:36:24 +0100

    add intel's defined exceptions

--- kernel/idt.c
+++ kernel/idt.c
@@ -1,7 +1,9 @@
 #include <idt.h>
+#include <monitor.h>
+#include <vga.h>
 #include <printf.h>
 
-#define IDT_COUNT 2
+#define IDT_COUNT 50
 
 struct idt_entry idt[IDT_COUNT];
 struct idt_ptr idt_p;
@@ -26,10 +28,25 @@ char *exception_names[] = {
             "Coprocessor Error"
 };
 
-void interrupt_handler(struct regs *r)
-{
+isr_handler exception_routines[] = {
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+};
+
+void interrupt_handler(struct isr_state *r) {
+    isr_handler handler;
+
     printf("INTERRUPT %d: %s\n", r->int_no, exception_names[r->int_no]);
-    for (;;);
+
+    handler = exception_routines[r->int_no];
+    if (handler) {
+        handler(r);
+    }
+    else {
+        panic("No handler for interrupt %d: %s\n", r->int_no, exception_names[r->int_no]);
+    }
 }
 
 void idt_set_gate(uint8_t num, uint32_t handler, uint16_t seg_selector, uint8_t flags) {
@@ -43,9 +60,39 @@ void idt_init() {
     idt_p.base = &idt;
     idt_p.limit = (sizeof(struct idt_entry) * IDT_COUNT - 1);
 
-
-    idt_set_gate(0, (uint32_t)isr0, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
-    idt_set_gate(1, (uint32_t)isr1, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    /* 32 Intel's defined Exceptions */
+    idt_set_gate(00, (uint32_t)isr0,  0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(01, (uint32_t)isr1,  0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(02, (uint32_t)isr2,  0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(03, (uint32_t)isr3,  0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(04, (uint32_t)isr4,  0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(05, (uint32_t)isr5,  0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(06, (uint32_t)isr6,  0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(07, (uint32_t)isr7,  0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(08, (uint32_t)isr8,  0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(09, (uint32_t)isr9,  0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(10, (uint32_t)isr10, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(11, (uint32_t)isr11, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(12, (uint32_t)isr12, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(13, (uint32_t)isr13, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(14, (uint32_t)isr14, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(15, (uint32_t)isr15, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(16, (uint32_t)isr16, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(17, (uint32_t)isr17, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(18, (uint32_t)isr18, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(19, (uint32_t)isr19, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(20, (uint32_t)isr20, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(21, (uint32_t)isr21, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(22, (uint32_t)isr22, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(23, (uint32_t)isr23, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(24, (uint32_t)isr24, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(25, (uint32_t)isr25, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(26, (uint32_t)isr26, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(27, (uint32_t)isr27, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(28, (uint32_t)isr28, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(29, (uint32_t)isr29, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(30, (uint32_t)isr30, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
+    idt_set_gate(31, (uint32_t)isr31, 0x8, IDT_PRESENT | IDT_RING0 | IDT_INTERRUPT_GATE);
 
     idt_load();
 }
--- kernel/include/idt.h
+++ kernel/include/idt.h
@@ -12,6 +12,26 @@
 #define IDT_INTERRUPT_GATE 14
 #define IDT_TRAP_GATE 15
 
+#define DIVIDE_ERROR
+#define DEBUG_EXCEPTION
+#define INTEL_RESERVED
+#define BREAKPOINT
+#define OVERFLOW
+#define BOUNDS_CHECK
+#define INVALID_OPCODE
+#define COPROCESSOR_NOT_AVAILABLE
+#define DOUBLE_FAULT
+#define COPROCESSOR_SEGMENT_OVERRUN
+#define INVALID_TSS
+#define SEGMENT_NOT_PRESENT
+#define STACK_EXCEPTION
+#define GP_EXCEPTION
+#define PAGE_FAULT
+#define INTEL_RESERVED
+#define COPROCESSOR_ERROR
+
+typedef void (*isr_handler)(struct isr_state *);
+
 /*
 flags:
       7                           0
@@ -55,9 +75,6 @@ struct isr_state
 
 void idt_init();
 void interrupt_handler(struct isr_state *);
-extern void idt_load();
-extern void isr0();
-extern void isr1();
 
 static inline void interrupt_disable() {
     __asm__ __volatile__("cli");
@@ -66,5 +83,39 @@ static inline void interrupt_disable() {
 static inline void interrupt_enable() {
     __asm__ __volatile__("sti");
 }
+
+extern void idt_load();
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
 
 #endif
